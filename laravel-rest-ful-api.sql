7d04c85741c879b8e3ce388edfde49a728b1ef80
Snoopy Dev <hengsok2202@gmail.com>
2022-12-25 19:22:13 +0700

    Skyfall

---
+++ laravel-rest-ful-api.sql
@@ -0,0 +1,279 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : PosgreSQL
+ Source Server Type    : PostgreSQL
+ Source Server Version : 140001
+ Source Host           : localhost:5432
+ Source Catalog        : laravel_rest_ful_api
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 140001
+ File Encoding         : 65001
+
+ Date: 25/12/2022 19:20:46
+*/
+
+
+-- ----------------------------
+-- Sequence structure for failed_jobs_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."failed_jobs_id_seq";
+CREATE SEQUENCE "public"."failed_jobs_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for migrations_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."migrations_id_seq";
+CREATE SEQUENCE "public"."migrations_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for personal_access_tokens_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."personal_access_tokens_id_seq";
+CREATE SEQUENCE "public"."personal_access_tokens_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for posts_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."posts_id_seq";
+CREATE SEQUENCE "public"."posts_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for users_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."users_id_seq";
+CREATE SEQUENCE "public"."users_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for failed_jobs
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."failed_jobs";
+CREATE TABLE "public"."failed_jobs" (
+  "id" int8 NOT NULL DEFAULT nextval('failed_jobs_id_seq'::regclass),
+  "uuid" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "connection" text COLLATE "pg_catalog"."default" NOT NULL,
+  "queue" text COLLATE "pg_catalog"."default" NOT NULL,
+  "payload" text COLLATE "pg_catalog"."default" NOT NULL,
+  "exception" text COLLATE "pg_catalog"."default" NOT NULL,
+  "failed_at" timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+;
+
+-- ----------------------------
+-- Records of failed_jobs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."migrations";
+CREATE TABLE "public"."migrations" (
+  "id" int4 NOT NULL DEFAULT nextval('migrations_id_seq'::regclass),
+  "migration" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "batch" int4 NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of migrations
+-- ----------------------------
+INSERT INTO "public"."migrations" VALUES (1, '2014_10_12_000000_create_users_table', 1);
+INSERT INTO "public"."migrations" VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
+INSERT INTO "public"."migrations" VALUES (3, '2019_08_19_000000_create_failed_jobs_table', 1);
+INSERT INTO "public"."migrations" VALUES (4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+INSERT INTO "public"."migrations" VALUES (5, '2022_12_24_164449_create_posts_table', 1);
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."password_resets";
+CREATE TABLE "public"."password_resets" (
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "token" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "created_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of password_resets
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for personal_access_tokens
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."personal_access_tokens";
+CREATE TABLE "public"."personal_access_tokens" (
+  "id" int8 NOT NULL DEFAULT nextval('personal_access_tokens_id_seq'::regclass),
+  "tokenable_type" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "tokenable_id" int8 NOT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "token" varchar(64) COLLATE "pg_catalog"."default" NOT NULL,
+  "abilities" text COLLATE "pg_catalog"."default",
+  "last_used_at" timestamp(0),
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of personal_access_tokens
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for posts
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."posts";
+CREATE TABLE "public"."posts" (
+  "id" int8 NOT NULL DEFAULT nextval('posts_id_seq'::regclass),
+  "title" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "content" text COLLATE "pg_catalog"."default" NOT NULL,
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of posts
+-- ----------------------------
+INSERT INTO "public"."posts" VALUES (4, 'Post 2', 'no content here', '2022-12-24 17:07:46', '2022-12-24 17:07:46');
+INSERT INTO "public"."posts" VALUES (5, 'Post 3', 'no content', '2022-12-24 17:08:03', '2022-12-24 17:08:03');
+INSERT INTO "public"."posts" VALUES (6, 'Post 4', 'no content', '2022-12-24 17:08:08', '2022-12-24 17:08:08');
+INSERT INTO "public"."posts" VALUES (7, 'Post 5', 'no content', '2022-12-24 17:08:11', '2022-12-24 17:08:11');
+INSERT INTO "public"."posts" VALUES (1, 'Post 1', 'no content', NULL, '2022-12-24 17:14:50');
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."users";
+CREATE TABLE "public"."users" (
+  "id" int8 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email_verified_at" timestamp(0),
+  "password" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "remember_token" varchar(100) COLLATE "pg_catalog"."default",
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."failed_jobs_id_seq"
+OWNED BY "public"."failed_jobs"."id";
+SELECT setval('"public"."failed_jobs_id_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."migrations_id_seq"
+OWNED BY "public"."migrations"."id";
+SELECT setval('"public"."migrations_id_seq"', 5, true);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."personal_access_tokens_id_seq"
+OWNED BY "public"."personal_access_tokens"."id";
+SELECT setval('"public"."personal_access_tokens_id_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."posts_id_seq"
+OWNED BY "public"."posts"."id";
+SELECT setval('"public"."posts_id_seq"', 7, true);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."users_id_seq"
+OWNED BY "public"."users"."id";
+SELECT setval('"public"."users_id_seq"', 1, false);
+
+-- ----------------------------
+-- Uniques structure for table failed_jobs
+-- ----------------------------
+ALTER TABLE "public"."failed_jobs" ADD CONSTRAINT "failed_jobs_uuid_unique" UNIQUE ("uuid");
+
+-- ----------------------------
+-- Primary Key structure for table failed_jobs
+-- ----------------------------
+ALTER TABLE "public"."failed_jobs" ADD CONSTRAINT "failed_jobs_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table migrations
+-- ----------------------------
+ALTER TABLE "public"."migrations" ADD CONSTRAINT "migrations_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Indexes structure for table password_resets
+-- ----------------------------
+CREATE INDEX "password_resets_email_index" ON "public"."password_resets" USING btree (
+  "email" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
+);
+
+-- ----------------------------
+-- Indexes structure for table personal_access_tokens
+-- ----------------------------
+CREATE INDEX "personal_access_tokens_tokenable_type_tokenable_id_index" ON "public"."personal_access_tokens" USING btree (
+  "tokenable_type" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST,
+  "tokenable_id" "pg_catalog"."int8_ops" ASC NULLS LAST
+);
+
+-- ----------------------------
+-- Uniques structure for table personal_access_tokens
+-- ----------------------------
+ALTER TABLE "public"."personal_access_tokens" ADD CONSTRAINT "personal_access_tokens_token_unique" UNIQUE ("token");
+
+-- ----------------------------
+-- Primary Key structure for table personal_access_tokens
+-- ----------------------------
+ALTER TABLE "public"."personal_access_tokens" ADD CONSTRAINT "personal_access_tokens_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table posts
+-- ----------------------------
+ALTER TABLE "public"."posts" ADD CONSTRAINT "posts_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Uniques structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_email_unique" UNIQUE ("email");
+
+-- ----------------------------
+-- Primary Key structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+/// get all posts
+Route::get("/posts", function () {
+    return Post::all();
+});
+
+/// insert to post
+Route::post("/posts/insert", function (){
+    return Post::create([
+        "title"=> request("title"),
+        "content" => request("content")
+    ]);
+});
+
+/// search post
+Route::get("/post/search/{id}", function (){
+    return Post::findOrFail(request("id"));
+});
+
+/// update a post
+Route::post("/post/update/{id}/{title}/{content}", function () {
+    $post = Post::find(request("id"));
+    $post->title = request("title");
+    $post->content = request("content");
+    $post->update();
+    return "Updated !";
+});
+
+/// delete post
+Route::post("/post/delete/{id}", function () {
+    return Post::where("id", request("id"))->firstorfail()->delete();
 });
